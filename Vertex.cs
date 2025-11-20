bdd98c00bbfede2afddd5abfd4ddde098b73a055
ALENOR <ilyalepa333@gmail.com>
2025-11-20 17:37:01 +0300

    I update main code VertexLogger. Add function BasicSetting

--- Vertex.cs
+++ Vertex.cs
@@ -5,44 +5,42 @@ namespace Vertex
         public class Data
         {
             public string[] LEVEL = { "[DEBUG]", "[WARN]", "[ERROR]" };
+            public string pathLog;
         }
         DateTime dateTime = DateTime.Now;
         Data dataLogger = new Data();
 
+        public string BasicSetting(string pathFile)
+        {
+            using (FileStream file = new FileStream(pathFile, FileMode.Create))
+            {
+                file.Close();
+            }
+            return pathFile;
+        }
         public void WriteText(string text, string pathFile)
         {
-            if (!File.Exists(pathFile))
-            {
-                File.Create(pathFile);
-            }
-            else
-            {
-                File.AppendAllText(pathFile, text + "\n");
-            }
-
+            File.AppendAllText(pathFile, text + "\n");
         }
-        public string Debug(string text, string pathFile)
+        public void Debug(string text, string pathFile)
         {
             string nowDay = $"{dateTime.Day}.{dateTime.Month}.{dateTime.Year} {dateTime.Hour}:{dateTime.Minute}";
-            string fullText = $"{dataLogger.LEVEL[0]} - {nowDay} | {text}";
+            string fullText = $"{text} {dataLogger.LEVEL[0]} | {nowDay}";
             WriteText(fullText, pathFile);
-            return fullText;
         }
 
-        public string Warn(string text, string pathFile)
+        public void Warn(string text, string pathFile)
         {
             string nowDay = $"{dateTime.Day}.{dateTime.Month}.{dateTime.Year} {dateTime.Hour}:{dateTime.Minute}";
-            string fullText = $"{dataLogger.LEVEL[1]} - {nowDay} | {text}";
+            string fullText = $"{text} {dataLogger.LEVEL[0]} | {nowDay}";
             WriteText(fullText, pathFile);
-            return fullText;
         }
 
-        public string Error(string text, string pathFile)
+        public void Error(string text, string pathFile)
         {
             string nowDay = $"{dateTime.Day}.{dateTime.Month}.{dateTime.Year} {dateTime.Hour}:{dateTime.Minute}";
-            string fullText = $"{dataLogger.LEVEL[2]} - {nowDay} | {text}";
+            string fullText = $"{text} {dataLogger.LEVEL[0]} | {nowDay}";
             WriteText(fullText, pathFile);
-            return fullText;
         }
     }
 }
