61be712376667dadf2a15ffd100709cc04e9a9f6
ALENOR <ilyalepa333@gmail.com>
2026-01-02 00:46:50 +0300

    I updated code and README.me :0

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # VertexLogger
-## VertexLogger - logger that allows you to log all actions in your project and automatically save them in a file
+## VertexLogger is a logger that allows you to log all the actions of your code. It is suitable for all projects, from Hello World to pet projects with a microservice architecture
 ![Alt text](Images/Image.jpg)
 
 ## Example of using VertexLogger:
 
 ![Alt text](Images/Preview.jpg)
 
-All methods:
-1) DEBUG 
+All VertexLogger methods:
+1) DEBUG
 2) WARN
 3) ERROR
+
+
 
 
--- Vertex.cs
+++ Vertex.cs
@@ -2,44 +2,37 @@ namespace Vertex
 {
     public class VertexLogger
     {
-        public class Data
-        {
-            public string[] LEVEL = { "[DEBUG]", "[WARN]", "[ERROR]" };
-            public string pathLog;
-        }
-        DateTime dateTime = DateTime.Now;
-        Data dataLogger = new Data();
 
-        public string BasicSetting(string pathFile)
+        private DateTime dateTime = DateTime.Now;
+        private string[] LEVEL = { "DEBUG", "WARN", "ERROR" };
+
+        private string TimeNow()
         {
-            using (FileStream file = new FileStream(pathFile, FileMode.Create))
-            {
-                file.Close();
-            }
-            return pathFile;
+            string nowDay = $"{dateTime.Day}.{dateTime.Month}.{dateTime.Year} {dateTime.Hour}:{dateTime.Minute}";
+            return nowDay;
         }
-        public void WriteText(string text, string pathFile)
+
+        private void WriteText(string text, string pathFile)
         {
             File.AppendAllText(pathFile, text + "\n");
         }
         public void Debug(string text, string pathFile)
         {
-            string nowDay = $"{dateTime.Day}.{dateTime.Month}.{dateTime.Year} {dateTime.Hour}:{dateTime.Minute}";
-            string fullText = $"{text} {dataLogger.LEVEL[0]} | {nowDay}";
+
+            string fullText = $"[{LEVEL[0]}] - {text}  | {TimeNow()}";
             WriteText(fullText, pathFile);
         }
 
         public void Warn(string text, string pathFile)
         {
-            string nowDay = $"{dateTime.Day}.{dateTime.Month}.{dateTime.Year} {dateTime.Hour}:{dateTime.Minute}";
-            string fullText = $"{text} {dataLogger.LEVEL[0]} | {nowDay}";
+
+            string fullText = $"[{LEVEL[1]}] - {text}  | {TimeNow()}";
             WriteText(fullText, pathFile);
         }
 
         public void Error(string text, string pathFile)
         {
-            string nowDay = $"{dateTime.Day}.{dateTime.Month}.{dateTime.Year} {dateTime.Hour}:{dateTime.Minute}";
-            string fullText = $"{text} {dataLogger.LEVEL[0]} | {nowDay}";
+            string fullText = $"[{LEVEL[2]}] - {text}  | {TimeNow()}";
             WriteText(fullText, pathFile);
         }
     }
