d4c9e7369321c292c1a5cb680ac07d256819432f
ALENOR <ilyalepa333@gmail.com>
2026-03-05 20:19:19 +0300

    A DataLogger.cs has been added and also modified VertexLogger.cs

---
+++ DataLogger.cs
@@ -0,0 +1,9 @@
+﻿public class DataLogger
+{
+    protected string[] level = ["DEBUG", "WARN", "ERROR"];
+    protected string text;
+    protected string time;
+    protected string pathFile;
+
+}
+
---
+++ VertexLogger.cs
@@ -0,0 +1,31 @@
+﻿public class VertexLogger : DataLogger
+{
+
+    private static string timeNow = $"{DateTime.Now.Date} - {DateTime.Now.Hour}:{DateTime.Now.Minute}";
+
+    public VertexLogger(string path)
+    {
+        base.pathFile = path;
+        base.time = timeNow;
+    }
+
+    public void Debug(string text)
+    {
+        base.text = text;
+        string result = $"{base.time} | [{base.level[0]}] - {base.text}";
+        File.AppendAllText(base.pathFile, result);
+    }
+    public void Warn(string text)
+    {
+        base.text = text;
+        string result = $"{base.time} | [{base.level[1]}] - {base.text}";
+        File.AppendAllText(base.pathFile, result);
+    }
+    public void Error(string text)
+    {
+        base.text = text;
+        string result = $"{base.time} | [{base.level[2]}] - {base.text}";
+        File.AppendAllText(base.pathFile, result);
+    }
+}
+
