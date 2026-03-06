ee40ff29c620f4f436b171c3b5d440977abd12b3
ALENOR <ilyalepa333@gmail.com>
2026-03-06 20:29:03 +0300

    Changed VertexLogger.cs again

--- VertexLogger.cs
+++ VertexLogger.cs
@@ -1,29 +1,30 @@
 ﻿public class VertexLogger : DataLogger
 {
 
-    private static string timeNow = $"{DateTime.Now.Date} - {DateTime.Now.Hour}:{DateTime.Now.Minute}";
+    private static string timeNow = $"{DateTime.Now.Day}.{DateTime.Now.Month}.{DateTime.Now.Year} - {DateTime.Now.Hour}:{DateTime.Now.Minute}";
 
-    public VertexLogger(string path)
+    public VertexLogger(string path): base(path, timeNow)
     {
-        base(path, timeNow);
+
     }
+    
 
     public void Debug(string text)
     {
         base.text = text;
-        string result = $"{base.time} | [{base.level[0]}] - {base.text}";
+        string result = $"{base.time} | [{base.level[0]}] - {base.text}\n";
         File.AppendAllText(base.pathFile, result);
     }
     public void Warn(string text)
     {
         base.text = text;
-        string result = $"{base.time} | [{base.level[1]}] - {base.text}";
+        string result = $"{base.time} | [{base.level[1]}] - {base.text}\n";
         File.AppendAllText(base.pathFile, result);
     }
     public void Error(string text)
     {
         base.text = text;
-        string result = $"{base.time} | [{base.level[2]}] - {base.text}";
+        string result = $"{base.time} | [{base.level[2]}] - {base.text}\n";
         File.AppendAllText(base.pathFile, result);
     }
 }
